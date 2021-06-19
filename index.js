402ab858fbf36f74bb32e06cca978dc956dcf441
manik21 <manikdhanjal217@gmail.com>
2021-06-20 00:18:27 +0530

    port change

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/scrapper/stop',async (req,res) => {
    browser.close();
     res.send('Browser instance and scrapping stopped')
 })
-const PORT = process.env.port||3000;
+const PORT = process.env.port||8080;
 app.listen(PORT,()=>{
     console.log('scrapper server listening at PORT:',PORT)
 })
